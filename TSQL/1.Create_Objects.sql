157868066e3821e4e0dfecd0dbacb3d9072beb17
Matt Smith <matt@matt40k.co.uk>
2015-05-18 23:46:18 +0100

    More progress

--- TSQL/1.Create_Objects.sql
+++ TSQL/1.Create_Objects.sql
@@ -5,6 +5,137 @@ CREATE TABLE [Staging].[MIS](
 )
 GO
 
+CREATE TABLE [Staging].[Edubase_Extract] (
+	[Address3] varchar(50) NULL
+	,[County (name)] varchar(50) NULL
+	,[CloseDate] varchar(50) NULL
+	,[EstablishmentName] varchar(100) NULL
+	,[EstablishmentNumber] varchar(50) NULL
+	,[OpenDate] varchar(50) NULL
+	,[EstablishmentStatus (name)] varchar(50) NULL
+	,[Gender (name)] varchar(50) NULL
+	,[HeadFirstName] varchar(50) NULL
+	,[HeadHonours] varchar(50) NULL
+	,[HeadLastName] varchar(50) NULL
+	,[HeadPreferredJobTitle] varchar(50) NULL
+	,[HeadTitle (name)] varchar(50) NULL
+	,[LastChangedDate] varchar(50) NULL
+	,[LA (name)] varchar(50) NULL
+	,[LA (code)] varchar(50) NULL
+	,[Locality] varchar(50) NULL
+	,[Postcode] varchar(50) NULL
+	,[PreviousEstablishmentNumber] int NULL
+	,[PreviousLA (name)] varchar(50) NULL
+	,[PreviousLA (code)] int NULL
+	,[ReasonEstablishmentClosed (name)] varchar(50) NULL
+	,[ReasonEstablishmentOpened (name)] varchar(50) NULL
+	,[StatutoryHighAge] int NULL
+	,[StatutoryLowAge] int NULL
+	,[Street] varchar(50) NULL
+	,[TelephoneNum] varchar(50) NULL
+	,[Town] varchar(50) NULL
+	,[TypeOfEstablishment (name)] varchar(50) NULL
+	,[UKPRN] int NULL
+	,[URN] int NULL
+	,[SchoolWebsite] varchar(100) NULL
+	,[OfficialSixthForm (name)] varchar(50) NULL
+	,[FEHEIdentifier] varchar(255) NULL
+	,[FurtherEducationType (name)] varchar(50) NULL
+	,[PhaseOfEducation (name)] varchar(50) NULL
+	,[AdmissionsPolicy (name)] varchar(50) NULL
+	,[Boarders (name)] varchar(50) NULL
+	,[BoardingEstablishment (name)] varchar(50) NULL
+	,[Diocese (name)] varchar(50) NULL
+	,[OfstedLastInsp] varchar(50) NULL
+	,[PercentageFSM] varchar(50) NULL
+	,[ReligiousCharacter (name)] varchar(50) NULL
+	,[SchoolCapacity] varchar(50) NULL
+	,[Trusts (name)] varchar(100) NULL
+	,[TrustSchoolFlag (name)] varchar(50) NULL
+	,[Federations (name)] varchar(150) NULL
+	,[FederationFlag (name)] varchar(25) NULL
+	,[SchoolSponsors (name)] varchar(25) NULL
+	,[SchoolSponsorFlag (name)] varchar(20) NULL
+	,[GSSLACode (name)] varchar(10) NULL
+	,[ASCHighestAge] int NULL
+	,[ASCLowestAge] int NULL
+	,[Boys2] int NULL
+	,[Boys3] int NULL
+	,[Boys4a] int NULL
+	,[Boys4b] int NULL
+	,[Boys4c] int NULL
+	,[Boys5] int NULL
+	,[Boys6] int NULL
+	,[Boys7] int NULL
+	,[Boys8] int NULL
+	,[Boys9] int NULL
+	,[Boys10] int NULL
+	,[Boys11] int NULL
+	,[Boys12] int NULL
+	,[Boys13] int NULL
+	,[Boys14] int NULL
+	,[Boys15] int NULL
+	,[Boys16] int NULL
+	,[Boys17] int NULL
+	,[Boys18] int NULL
+	,[Boys19plus] int NULL
+	,[CensusDate] date NULL
+	,[Girls2andUnder] int NULL
+	,[Girls3] int NULL
+	,[Girls4a] int NULL
+	,[Girls4b] int NULL
+	,[Girls4c] int NULL
+	,[Girls5] int NULL
+	,[Girls6] int NULL
+	,[Girls7] int NULL
+	,[Girls8] int NULL
+	,[Girls9] int NULL
+	,[Girls10] int NULL
+	,[Girls11] int NULL
+	,[Girls12] int NULL
+	,[Girls13] int NULL
+	,[Girls14] int NULL
+	,[Girls15] int NULL
+	,[Girls16] int NULL
+	,[Girls17] int NULL
+	,[Girls18] int NULL
+	,[Girls19plus] int NULL
+	,[NumberOfBoys] int NULL
+	,[NumberOfGirls] int NULL
+	,[NumberOfPupils] int NULL
+	,[AdministrativeWard (name)] varchar(60) NULL
+	,[GOR (name)] varchar(50) NULL
+	,[LSOA (name)] varchar(50) NULL
+	,[MSOA (name)] varchar(50) NULL
+	,[ParliamentaryConstituency (name)] varchar(50) NULL
+	,[UrbanRural (name)] varchar(50) NULL
+	,[Easting] int NULL
+	,[Northing] int NULL
+	,[OfstedSpecialMeasures (name)] varchar(50) NULL
+	,[SpecialClasses (name)] varchar(50) NULL
+	,[TeenMothPlaces] int NULL
+	,[TeenMoth (name)] varchar(50) NULL
+	,[CCF (name)] varchar(50) NULL
+	,[FTProv (name)] varchar(50) NULL
+	,[EdByOther (name)] varchar(50) NULL
+	,[EBD (name)] varchar(50) NULL
+	,[SENPRU (name)] varchar(50) NULL
+	,[SEN1 (name)] varchar(300) NULL
+	,[SEN2 (name)] varchar(330) NULL
+	,[SEN3 (name)] varchar(75) NULL
+	,[Section41Approved (name)] varchar(15) NULL
+)
+GO
+
+CREATE TABLE [Staging].[Edubase_Links] (
+	[URN] int NULL
+	,[LinkURN] int NULL
+	,[LinkName] varchar(150) NULL
+	,[LinkType] varchar(65) NULL
+	,[LinkEstablishedDate] date NULL
+)
+GO
+
 IF ((select count(1) from sys.tables where name='Contract' and schema_name(schema_id) = 'dbo') > 0)
 	DROP TABLE [dbo].[Contract]
 GO
@@ -78,3 +209,19 @@ select
 from
 	[dbo].[Company]
 GO
+
+IF ((select count(1) from sys.views where name='vw_School' and schema_name(schema_id) = 'dbo') > 0)
+	DROP VIEW [Staging].[vw_School]
+GO
+CREATE VIEW [Staging].[vw_School]
+AS
+select distinct
+	LA_CODE=[la (code)]
+	,LA_NAME=[la (name)]
+	,SCH_CODE=establishmentnumber
+	,SCH_NAME=establishmentname
+	,SCH_POSTCODE=Postcode
+	,SCH_Easting=Easting
+	,SCH_Northing=Northing
+from
+	staging.edubase_extract
